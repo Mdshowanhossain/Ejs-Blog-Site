16cc3d4777d677193627db2fcdf4d06e65f32b4d
unknown <Mdshowanhossain11@gmail.com>
2021-09-26 18:13:45 +0600

    Read-Post

--- routes/home.js
+++ routes/home.js
@@ -5,31 +5,32 @@ const Blog = require('../models/blogSchema');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('home');
-})
+// router.get('/', (req, res) => {
+//     res.render('home');
+// })
+
+
 
 router.get('/', async (req, res) => {
-
     try {
-        const blogs = [
-            { category: 'sohan', title: 'sohan', description: 'sohan', },
-            { category: 'sohan', title: 'sohan', description: 'sohan', },
-            { category: 'sohan', title: 'sohan', description: 'sohan', },
-            { category: 'sohan', title: 'sohan', description: 'sohan', },
-            { category: 'sohan', title: 'sohan', description: 'sohan', },
-        ]
+        const blogs = await Blog.find();
         res.render('home', { blogs: blogs })
-
-
-    } catch (err) {
+    }
+    catch (err) {
         console.log(err)
     }
+});
 
 
+router.get('/readBlog/:id', async (req, res) => {
+    try {
+        const findBlog = await Blog.findById({ _id: req.params.id });
+        res.render('readpost', { findBlog: findBlog });
+        // console.log(req.params.id)
+    } catch (err) {
 
+    }
 })
-
 
 
 
--- routes/read.js
+++ routes/read.js
@@ -1,10 +1,18 @@
 const express = require('express');
 
+const Blog = require('../models/blogSchema');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('readpost');
-})
+// router.get('/readBlog/:id', async (req, res) => {
+//     try {
+//         const findBlog = await Blog.findById({ _id: req.params.id });
+//         res.render('readpost', { findBlog: findBlog });
+//         console.log(req.params.id)
+//     } catch (err) {
+
+//     }
+// })
 
 
 module.exports = router;
