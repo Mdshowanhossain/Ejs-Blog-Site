bd752110b8fd1b407734f9047efd291fd649c8ed
Mdshowanhossain <mdshowanhossain11@gmail.com>
2021-10-20 18:36:54 +0600

    DEPLOY

--- Database/db.js
+++ Database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/ourBlog')
+mongoose.connect('mongodb+srv://FirstData:FirstData1@cluster0.f2pep.mongodb.net/myFirstDatabase?authSource=admin&replicaSet=atlas-qzci16-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true')
     .then((res) => {
         console.log('Your Database Connection Succesful')
     })
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const popularBlog = require('./routes/popularBlog');
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser());
 app.use(cookieParser());
 app.set('view engine', 'ejs');
